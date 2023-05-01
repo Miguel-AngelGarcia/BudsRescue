063e7388151882c67e531c2450f9c23e0c90f50a
Miguel-AngelGarcia <mgar574@wgu.edu>
2023-05-01 16:30:03 -0400

    Worked on projectile-badGuy collision logic

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ class Projectile {
     (this.position = position),
       //this could be something to help make a power-up
       (this.velocity = velocity),
-      (this.radius = 3);
+      (this.radius = 5);
   }
 
   draw() {
@@ -101,10 +101,113 @@ class Projectile {
   }
 }
 
+class BadGuy {
+  constructor({ position }) {
+    //for moving around
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    //this.image =
+    const image = new Image();
+    image.src = "./Images/BadGuy.png";
+
+    //will listen for when image fully loads
+    //when it does, will set the properties of image to these
+    image.onload = () => {
+      const scale = 0.5;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+
+      //will place character at bottom
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
+
+  draw() {
+    context.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.height,
+      this.height
+    );
+  }
+
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+    }
+  }
+}
+
+class Grid {
+  //each grid will have an array of badGuys
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+
+    this.velocity = {
+      x: 2,
+      y: 0,
+    };
+
+    //making an array of bad guys
+    this.badGuyGroup = [];
+
+    //looping to create bad guys, minimum of 2 rows
+    let rows = Math.floor(Math.random() * 4) + 2;
+    let columns = Math.floor(Math.random() * 6) + 5;
+
+    this.width = columns * 70;
+
+    for (let xAxis = 0; xAxis < columns; xAxis++) {
+      for (let yAxis = 0; yAxis < rows; yAxis++) {
+        this.badGuyGroup.push(
+          new BadGuy({
+            //need to pass position so they dont spawn on top of each other
+            position: {
+              x: xAxis * 70,
+              y: yAxis * 55,
+            },
+          })
+        );
+      }
+    }
+  }
+
+  //update will need to
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    //effects movement per frame
+    //we saw that instead of going down x pixels, it just drops down completely
+    this.velocity.y = 0;
+    //we didnt get the grid of bad guys to bounce off right side of screen.
+    //Why? We enever set a width for the grid
+    // -> this.width = columns * 70; (70 is width of column holding bad guy)
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 55; //55 is height of badGuy
+    }
+  }
+}
+
 //we noticed, the image never showed up.
 //Why? It takes time to load.
 const player = new Player();
 const projectiles = [];
+const grids = [];
 
 const keys = {
   a: {
@@ -120,6 +223,12 @@ const keys = {
   },
 };
 
+//we want the grid to spawn at different intervals
+//
+
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 1000 + 500);
+
 //to address issue, we use this animation loop
 //this will update our image continously
 function animate() {
@@ -128,6 +237,71 @@ function animate() {
   //filling background
   context.fillStyle = "#f6d7b0";
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  //
+  grids.forEach((grid, gIndex) => {
+    grid.update();
+    grid.badGuyGroup.forEach((badGuy, bgIndex) => {
+      badGuy.update({ velocity: grid.velocity });
+
+      projectiles.forEach((projectile, pIndex) => {
+        //detects for colision
+        //checks if top of projectile reaches bottom of badGuy
+        // if yes, removes BadGuy from BagGuyGrop
+        let projectWithinHeight =
+          projectile.position.y - projectile.radius <=
+          badGuy.position.y + badGuy.height;
+
+        let projectileWithinBaddy =
+          projectile.position.x + projectile.radius >= badGuy.position.x &&
+          projectile.position.x - projectile.radius <=
+            badGuy.position.x + badGuy.width;
+
+        let projectileAboveBadGuyBottom =
+          projectile.position.y + projectile.radius >= badGuy.position.y;
+        if (
+          projectWithinHeight &
+          projectileWithinBaddy &
+          projectileAboveBadGuyBottom
+        ) {
+          setTimeout(() => {
+            //this confirms the badGuy we hit in in the appropriate array
+            const badGuyFound = grid.badGuyGroup.find((badGuy2) => {
+              return badGuy2 === badGuy;
+            });
+
+            //needed same for projectile
+            const projectileFound = projectiles.find((projectile2) => {
+              return projectile2 === projectile;
+            });
+
+            //need to remove projectile and BadGuy
+            if (badGuyFound && projectileFound) {
+              grid.badGuyGroup.splice(bgIndex, 1);
+              projectiles.splice(pIndex, 1);
+
+              //adjusting columns when they get shot down and eliminated
+              //making sure grid bounces on new width not old width
+              if (grid.badGuyGroup.length > 0) {
+                const leftMostBadGuy = grid.badGuyGroup[0];
+                const rightMostBadGuy =
+                  grid.badGuyGroup[grid.badGuyGroup.length - 1];
+
+                grid.width =
+                  rightMostBadGuy.position.x -
+                  leftMostBadGuy.position.x +
+                  rightMostBadGuy.width;
+                grid.position.x = leftMostBadGuy.position.x;
+                //removes grid if empty
+              } else {
+                grids.splice(gIndex, 1);
+              }
+            }
+          }, 0);
+        }
+      });
+    });
+  });
 
   //will animate projectiles
   projectiles.forEach((projectile, index) => {
@@ -155,8 +329,17 @@ function animate() {
   } else {
     player.velocity.x = 0;
   }
-}
 
+  //spawn enemies at every n frames
+  if (frames % randomInterval === 0) {
+    grids.push(new Grid());
+    randomInterval = Math.floor(Math.random() * 1000 + 500);
+    //might get weird values if we let frames go unchecked
+    //set it to 0, will still let grid go down
+    frames = 0;
+  }
+  frames++;
+}
 animate();
 
 //will add listener for our moveaament keys
@@ -178,7 +361,7 @@ addEventListener("keydown", ({ key }) => {
           },
           velocity: {
             x: 0,
-            y: -2,
+            y: -4,
           },
         })
       );
